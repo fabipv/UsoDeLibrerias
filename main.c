9db02ea842d20e4aa09736227ee1af1686220634
fabipv <fabianapinavera13@gmail.com>
2023-12-12 22:21:30 -0300

    Update main.c and add Ahorcado.exe

--- main.c
+++ main.c
@@ -16,7 +16,11 @@ bool keyPressed(int vKey) {
     return GetAsyncKeyState(vKey) & 0x8000;
 }
 
-void drawRectangle(int x, int y) {
+void dibujoInicial(int x, int y) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    // Cambiar el color a verde claro para el dibujo inicial
+    SetConsoleTextAttribute(hConsole, 13);
+
     gotoxy(x, y);     printf("   _______________________________");
     gotoxy(x, y + 1); printf("  |    _____  _    ___   _        |                           _");
     gotoxy(x, y + 2); printf("  |   |  ___|| |  / _ \\ | |       |                          | |");
@@ -27,19 +31,36 @@ void drawRectangle(int x, int y) {
     gotoxy(x, y + 7); printf("  |                               |");
     gotoxy(x, y + 8); printf(" _|_________                     /|\\");
     gotoxy(x, y + 9); printf("                                / | \\");
-    gotoxy(x, y + 10); printf("			          / \\");
-    gotoxy(x, y + 11); printf("				 /   \\");
+    gotoxy(x, y + 10); printf("			           / \\");
+    gotoxy(x, y + 11); printf("				  /   \\");
+
+    // Restaurar el color original de la consola
+    SetConsoleTextAttribute(hConsole, 7);
 }
 
-void drawOptions(int x, int y, int selected) {
+void opciones(int x, int y, int selected) {
+
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    // Cambiar el color a cian para las opciones
+    SetConsoleTextAttribute(hConsole, 11);
+
     gotoxy(x + 6, y + 4); printf(selected == 1 ? "> JUGAR   " : "  JUGAR   ");
-    gotoxy(x + 5, y + 6); printf(selected == 2 ? "> PUNTAJES" : "  PUNTAJES");
-    gotoxy(x + 4, y + 8); printf(selected == 3 ? "> INSTRUCCIONES" : "  INSTRUCCIONES");
+    gotoxy(x + 6, y + 6); printf(selected == 2 ? "> PUNTAJES" : "  PUNTAJES");
+    gotoxy(x + 6, y + 8); printf(selected == 3 ? "> INSTRUCCIONES" : "  INSTRUCCIONES");
     gotoxy(x + 6, y + 10); printf(selected == 4 ? "> SALIR   " : "  SALIR   ");
+
+    // Restaurar el color original de la consola
+    SetConsoleTextAttribute(hConsole, 7);
 }
 
 int main() {
     system("cls");
+
+    // Ocultar el cursor para evitar que se muestre la línea parpadeante
+    CONSOLE_CURSOR_INFO cursorInfo;
+    cursorInfo.dwSize = 100;
+    cursorInfo.bVisible = FALSE;
+    SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
 
     int opcion = 1;
 
@@ -52,8 +73,8 @@ int main() {
 
         system("cls");
 
-        drawRectangle(10, 5);
-        drawOptions(30, 15, opcion);
+        dibujoInicial(10, 5);
+        opciones(30, 15, opcion);
 
         Sleep(100);
 
